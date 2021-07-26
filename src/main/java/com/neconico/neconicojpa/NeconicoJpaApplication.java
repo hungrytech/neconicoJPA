83fe15b568a50e52f60bcead54f1bbe8da845d1c
hungrytech <xorals9448@gmail.com>
2021-07-27 01:52:38 +0900

    Initial commit

---
+++ src/main/java/com/neconico/neconicojpa/NeconicoJpaApplication.java
@@ -0,0 +1,13 @@
+package com.neconico.neconicojpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeconicoJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NeconicoJpaApplication.class, args);
+    }
+
+}
